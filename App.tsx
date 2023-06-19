726689c937f31a5e15f848e395d1b1638dce2af2
Sun Yichun <sunyichun@bigo.sg>
2023-06-19 15:45:51 +0800

    feat: add tab bottom

--- App.tsx
+++ App.tsx
@@ -5,32 +5,40 @@ import nearbyScreen from "./src/screens/NearbyScreen";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FavouriteScreen from "./src/screens/FavouriteScreen";
 import NearbyScreen from "./src/screens/NearbyScreen";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {HeartIcon, MapIcon} from "react-native-heroicons/outline";
 
 type RootParamList = {
     FavouriteScreen: undefined
     NearbyScreen: undefined
     Screen3: { paramB: string; paramC: number }
 }
+
 function App(): JSX.Element {
     const Stack = createNativeStackNavigator();
-
+    const Tab = createBottomTabNavigator<RootParamList>();
     const Root = createNativeStackNavigator<RootParamList>();
     return (
-
-        // <View className="flex-1 items-center justify-center bg-white">
-        //    <Test></Test>
-        // </View>
         <NavigationContainer>
-            <Root.Navigator>
-                <Root.Screen name="FavouriteScreen" component={FavouriteScreen} />
-                <Root.Screen name="NearbyScreen" component={NearbyScreen} />
-            </Root.Navigator>
+            <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName={'FavouriteScreen'}>
+                <Tab.Screen name="FavouriteScreen" component={FavouriteScreen} options={{
+                    title: 'Favourite',
+                    headerShown: false,
+                    tabBarIcon: ({color, size}) => (
+                        <HeartIcon size={size} color={color}/>
+                    ),
+                    tabBarLabelPosition: 'beside-icon',
+                }}/>
+                <Tab.Screen name="NearbyScreen" component={NearbyScreen} options={{
+                    title: 'Nearby',
+                    headerShown: false,
+                    tabBarIcon: ({color, size}) => (
+                        <MapIcon size={size} color={color}/>
+                    ),
+                    tabBarLabelPosition: 'beside-icon',
+                }}/>
+            </Tab.Navigator>
         </NavigationContainer>
-        // <NavigationContainer>
-        //     <Stack.Navigator>
-        //         <Stack.Screen name="Home" component={nearbyScreen} />
-        //     </Stack.Navigator>
-        // </NavigationContainer>
     );
 }
 
--- src/screens/FavouriteScreen.tsx
+++ src/screens/FavouriteScreen.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Text, View} from "react-native";
-
+import {HeartIcon} from "react-native-heroicons/outline";
 const FavouriteScreen = () => {
     return (
         <View>
             <Text>FavouriteScreen</Text>
+            <HeartIcon size={24}/>
         </View>
     );
+
 
 
 }
