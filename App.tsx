a0b9c8da69d9605e123736c90d341c1314de301f
Sun Yichun <sunyichun@bigo.sg>
2023-06-19 10:56:52 +0800

    feat: add tailwind

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Text, View,} from 'react-native';
+import {StatusBar, Text, View,} from 'react-native';
 
 
 function App(): JSX.Element {
     return (
-        <View>
-            <Text>Test</Text>
+        // <View>
+        //     <Text>Test</Text>
+        // </View>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>Open up App.js to start working on your app!</Text>
         </View>
     );
 }
