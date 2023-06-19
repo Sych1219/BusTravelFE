850fbbd99252aa2e3a119d183b0c37ba886b13dc
Sun Yichun <sunyichun@bigo.sg>
2023-06-19 10:48:28 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View,} from 'react-native';
+
+
+function App(): JSX.Element {
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+}
+
+
+export default App;
